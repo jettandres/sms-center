2e5f2daf47185a1ad286d04902394ebb7510a23c
jettandres <jettrobin.andres@gmail.com>
2024-06-27 20:14:09 +0800

    Add first integration test
    This one still fails as intended

---
+++ server_integration_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	store := NewInMemoryStore()
+	server := NewSmsServer(store)
+
+	fromNumber := "0906765432"
+	toNumber := "0916123456"
+
+	server.ServeHTTP(httptest.NewRecorder(), newInsertSmsRequest(fromNumber, toNumber, "hello integ"))
+	server.ServeHTTP(httptest.NewRecorder(), newInsertSmsRequest(fromNumber, toNumber, "hello again integ"))
+	server.ServeHTTP(httptest.NewRecorder(), newInsertSmsRequest(fromNumber, toNumber, "hello once more integ"))
+
+	t.Run("retrieve ALL stored messages", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/sms", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		var apiResp GetAllSmsResponse
+		err := json.NewDecoder(response.Body).Decode(&apiResp)
+
+		if err != nil {
+			t.Errorf("Unable to parse response. Error: %s", err.Error())
+		}
+
+		if len(apiResp.Data.Sms) != 3 {
+			t.Error("Unable to retrieve any messages. Must have the 3 messages inserted")
+		}
+	})
+
+	t.Run("retrieve ALL messages of a number", func(t *testing.T) {})
+
+	t.Run("view a message", func(t *testing.T) {})
+}
+
+func newInsertSmsRequest(fromNumber string, toNumber string, body string) *http.Request {
+	reqBody := SmsPayload{
+		From: fromNumber,
+		Body: body,
+	}
+	payload, _ := json.Marshal(reqBody)
+	return httptest.NewRequest(http.MethodPost, fmt.Sprintf("/sms/%s", toNumber), strings.NewReader(string(payload)))
+}
