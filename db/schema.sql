0754f718b848119c1190077656d71bf974d6e569
jettandres <jettrobin.andres@gmail.com>
2024-06-30 18:11:01 +0800

    Finalize database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,11 @@
 CREATE TABLE IF NOT EXISTS "schema_migrations" (version varchar(128) primary key);
+CREATE TABLE sms_messages (
+    id TEXT PRIMARY KEY,
+    inserted_at TEXT,
+    body TEXT,
+    sender TEXT,
+    receiver TEXT
+);
 -- Dbmate schema migrations
+INSERT INTO "schema_migrations" (version) VALUES
+  ('20240630033905');
