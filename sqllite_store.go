d2059c3a4ff0c31ae85d78c0efa27cbf95d79560
jettandres <jettrobin.andres@gmail.com>
2024-07-03 08:52:27 +0800

    Refactor database/sql usage to sqlx
    Saved a few lines of code and struct mapping was pretty convenient

--- sqllite_store.go
+++ sqllite_store.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type SqliteStore struct {
 	driverName string
 	dsn        string
-	DB         *sql.DB
+	DB         *sqlx.DB
 }
 
 func NewSqliteStore() *SqliteStore {
@@ -21,7 +21,7 @@ func NewSqliteStore() *SqliteStore {
 		dsn:        os.Getenv("SQL_SQLITE_DSN"),
 	}
 
-	db, err := sql.Open(store.driverName, store.dsn)
+	db, err := sqlx.Open(store.driverName, store.dsn)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -33,7 +33,7 @@ func NewSqliteStore() *SqliteStore {
 func (store *SqliteStore) GetAllSms() ([]Sms, error) {
 	allSms := make([]Sms, 0)
 
-	stmt, err := store.DB.Prepare("SELECT id, body, sender, receiver, inserted_at FROM sms_messages")
+	stmt, err := store.DB.Preparex("SELECT * FROM sms_messages")
 	defer stmt.Close()
 
 	if err != nil {
@@ -41,20 +41,11 @@ func (store *SqliteStore) GetAllSms() ([]Sms, error) {
 		panic(err.Error())
 	}
 
-	rows, err := stmt.Query()
-	defer rows.Close()
+	err = stmt.Select(&allSms)
 
 	if err != nil {
 		fmt.Printf("[SQLITE_STORE][QUERY] Error: %s", err.Error())
 		panic(err.Error())
-	}
-
-	for rows.Next() {
-		var sms Sms
-		if err := rows.Scan(&sms.Id, &sms.Body, &sms.Sender, &sms.Receiver, &sms.Inserted_at); err != nil {
-			return nil, fmt.Errorf("[GET_ALL_SMS][ERROR] %s", err.Error())
-		}
-		allSms = append(allSms, sms)
 	}
 
 	return allSms, nil
@@ -63,7 +54,7 @@ func (store *SqliteStore) GetAllSms() ([]Sms, error) {
 func (store *SqliteStore) GetAllSmsFromSender(sender string) ([]Sms, error) {
 	allSmsFromSender := make([]Sms, 0)
 
-	stmt, err := store.DB.Prepare("SELECT id, body, sender, receiver, inserted_at FROM sms_messages WHERE sender = ?")
+	stmt, err := store.DB.Preparex("SELECT * FROM sms_messages WHERE sender = ?")
 	defer stmt.Close()
 
 	if err != nil {
@@ -71,27 +62,18 @@ func (store *SqliteStore) GetAllSmsFromSender(sender string) ([]Sms, error) {
 		panic(err.Error())
 	}
 
-	rows, err := stmt.Query(sender)
-	defer rows.Close()
+	err = stmt.Select(&allSmsFromSender, sender)
 
 	if err != nil {
 		fmt.Printf("[SQLITE_STORE][QUERY] Error: %s", err.Error())
 		panic(err.Error())
 	}
 
-	for rows.Next() {
-		var sms Sms
-		if err := rows.Scan(&sms.Id, &sms.Body, &sms.Sender, &sms.Receiver, &sms.Inserted_at); err != nil {
-			return nil, fmt.Errorf("[GET_ALL_SMS_FROM_SENDER][ERROR] %s", err.Error())
-		}
-		allSmsFromSender = append(allSmsFromSender, sms)
-	}
-
 	return allSmsFromSender, nil
 }
 
 func (store *SqliteStore) GetSmsById(id string) (Sms, error) {
-	stmt, err := store.DB.Prepare("SELECT id, body, sender, receiver, inserted_at FROM sms_messages WHERE id = ?")
+	stmt, err := store.DB.Preparex("SELECT * FROM sms_messages WHERE id = ?")
 	defer stmt.Close()
 
 	if err != nil {
@@ -100,7 +82,7 @@ func (store *SqliteStore) GetSmsById(id string) (Sms, error) {
 	}
 
 	var sms Sms
-	err = stmt.QueryRow(id).Scan(&sms.Id, &sms.Body, &sms.Sender, &sms.Receiver, &sms.Inserted_at)
+	err = stmt.Get(&sms, id)
 
 	if err != nil {
 		fmt.Printf("[GET_SMS_BY_ID][QUERY] Error: %s", err.Error())
@@ -111,7 +93,7 @@ func (store *SqliteStore) GetSmsById(id string) (Sms, error) {
 }
 
 func (store *SqliteStore) InsertSms(sender string, receiver string, body string) (Sms, error) {
-	stmt, err := store.DB.Prepare("INSERT INTO sms_messages (id, inserted_at, body, sender, receiver) VALUES (?,datetime(),?,?,?)")
+	stmt, err := store.DB.Preparex("INSERT INTO sms_messages (id, inserted_at, body, sender, receiver) VALUES (?,datetime(),?,?,?)")
 	defer stmt.Close()
 
 	if err != nil {
